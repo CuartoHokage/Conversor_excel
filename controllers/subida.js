3d1c440903d18f0118d20f20a5c2833020a579d2
CuartoHokage <jaime199505@hotmail.com>
2022-06-09 15:19:47 -0500

    Correccion de limitacion de caracteres

--- controllers/subida.js
+++ controllers/subida.js
@@ -27,7 +27,10 @@ function verExcel(ruta){
         //console.log(editor.slice(2, -1))
         //console.log(element)
         lista.N_PLU=dataExcel[index].N_Plu
+        //en caso de tener comas en la descripcion se eliminan para no dañar formato .CSV
         lista.Descripcion=(dataExcel[index].Descripcion).replace(',', ' ')
+        //limitacion de caracteres por confirmar
+        //lista.Descripcion=((dataExcel[index].Descripcion).replace(',', ' ')).substr(0,30)
         lista["Precio Etiqueta"]=precio
         lista.flag=dataExcel[index].Barra.substr(0,2)
         lista.plucode=dataExcel[index].Barra.slice(2)
